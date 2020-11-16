fc84d579242e31c156754a5f87a8317034ba6f51
Lippo <filippo.nardini@studio.unibo.it>
2020-11-16 16:25:27 +0100

    Modiefiechie

--- src/main/scala/items/Items2.scala
+++ src/main/scala/items/Items2.scala
@@ -12,15 +12,13 @@ object Items2 extends App {
     player: Player
   )
 
-  implicit val myContext: Context = Context {
-    new Player {
-      override def bag: Set[Item] = Set()
+  implicit val myContext: Context = Context(new Player {
+    override def bag: Set[Item] = Set()
 
-      override def health: Int = 100
+    override def health: Int = 100
 
-      override def toString: String = "Player object"
-    }
-  }
+    override def toString: String = "Player object"
+  })
 
   // Azione rappresenta univocamente la possibilità di effettuare una mossa all'interno del gioco.
   // Ad un unica azione possono corrispondere più verbi.
@@ -38,26 +36,25 @@ object Items2 extends App {
   // Event rappresenta il risultato POSITIVO di un'azione del giocatore.
   // Idealmente dovrebbe essere un oggetto che codifica le modifiche da fare
   // al player, all'environment, allo spazio, a tutto.
-  trait Event
+  trait Effect
 
-  object Events {
-    case class Store(item: Item) extends Event
-    case class RefillHealth(value: Int, player: Player) extends Event
-    case class Hit(what: Entity) extends Event
-    case class LoseHealth(value: Int) extends Event
+  object Effects {
+    case class Store(item: Item) extends Effect
+    case class RefillHealth(value: Int, player: Player) extends Effect
+    case class Hit(what: Entity) extends Effect
+    case class LoseHealth(value: Int, player: Player) extends Effect
   }
-
 
   trait Entity
   trait Item extends Entity {
-    def use(action: TransitiveAction): Option[Event]
+    def use(action: TransitiveAction): Option[Effect]
   }
 
-  type Property = PartialFunction[(TransitiveAction, Item), Event]
+  type Property = PartialFunction[(TransitiveAction, Item), Effect]
 
   object Item {
     def byProperty(property: Property): Item = new Item { self =>
-      override def use(action: TransitiveAction): Option[Event] = property.lift((action, self))
+      override def use(action: TransitiveAction): Option[Effect] = property.lift((action, self))
     }
 
     def byProperties(properties: Property*): Item = {
@@ -66,30 +63,41 @@ object Items2 extends App {
   }
 
   def canBeStored: Property = {
-    case (Actions.Take, item) => Events.Store(item)
+    case (Actions.Take, item) => Effects.Store(item)
   }
 
-  def custom(action: TransitiveAction)(createEvent: (Item, Context) => Event)(implicit context: Context): Property = {
-    case (_action, item) if _action == action => createEvent(item, context)
+//  type Property = PartialFunction[(TransitiveAction, Item), Effect]
+  def custom(action: TransitiveAction)(createEffect: (Item, Context) => Effect)(implicit context: Context): Property = {
+    case (_action, item) if _action == action => createEffect(item, context)
   }
+//    new PartialFunction[(TransitiveAction, Item), Effect] {
+//      override def apply(v1: (TransitiveAction, Item)): Effect = v1 match {
+//        case (_action, item) if _action == action => createEffect(item, context)
+//      }
+//
+//      override def isDefinedAt(x: (TransitiveAction, Item)): Boolean = x._1 == action
+//    }
+
 
 //  def food(createEvent: Context => Event)(implicit context: Context): Property = {
 //    case (Actions.Eat, _) => createEvent(context)
 //  }
-  def food(createEvent: (Item, Context) => Event): Property =
+  def food(createEvent: (Item, Context) => Effect): Property =
     custom(Actions.Eat)(createEvent)
 
   val apple = Item.byProperties(
     canBeStored,
-    food { case (_, Context(player)) => Events.RefillHealth(10, player) },
-    custom(Actions.Hit) { case (item, _) =>
-      Events.Hit(item)
-    }
+    food {
+      case (_, Context(player)) => Effects.RefillHealth(10, player)
+    },
+//    custom(Actions.Eat) { case (_, Context(player)) =>
+//      Effects.LoseHealth(10, player)
+//    }
   )
 
   val badApple = Item.byProperties(
     canBeStored,
-    food { case (_, Context(player)) => Events.LoseHealth(30) }
+    food { case (_, Context(player)) => Effects.LoseHealth(30, player) }
   )
 
   case class ResolverResult(
@@ -98,9 +106,7 @@ object Items2 extends App {
     item: Item
   )
 
-
-
-  def interpreter(resolverResult: ResolverResult)(implicit context: Context): Either[String, Event] = {
+  def interpreter(resolverResult: ResolverResult): Either[String, Effect] = {
     val ResolverResult(action, _, item) = resolverResult
     (item use action).toRight("You can't do such action.")
   }
